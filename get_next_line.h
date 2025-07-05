e937bae5297754f969f8b54526c1a738f8d22e4b
Huriye Beyza Torun <htorun@k1m12s03.42istanbul.com.tr>
2025-07-05 19:05:58 +0300

    fix

--- get_next_line.h
+++ get_next_line.h
@@ -12,7 +12,7 @@ char	*get_next_line(int fd);
 char	*ft_strjoin(const char *s1, char const *s2);
 char	*ft_strdup(const char *s);
 
-size_t	*ft_strrchr(char *s, int c);
+size_t	*ft_strchr(char *s, int c);
 size_t	ft_strlen(const char *s);
 
 #endif
