f397d587a97989d6a72ee8eeb5543c56b49b7dc7
Huriye Beyza Torun <htorun@k1m12s03.42istanbul.com.tr>
2025-07-05 19:08:56 +0300

    double pointer using fix

--- get_next_line.c
+++ get_next_line.c
@@ -1,41 +1,36 @@
 #include "get_next_line.h"
 #include <stdlib.h>
 
-static char	*newline(char *save)
+static char	*newline(char **save)
 {
 	char	*line;
 	char	*newline;
 	int		len;
 	char	*res;
 	int		i;
-    char *temp;
+	char	*temp;
+
 	if (!save)
 		return (NULL);
-	newline = ft_strrchr(save, '\n'); 
+	newline = ft_strrchr(*save, '\n');
 	if (newline)
 	{
-		len = newline - save + 1;
+		len = newline - *save + 1;
 		res = malloc(sizeof(char) * (len + 1));
 		if (!res)
 			return (NULL);
-		while ( i < len)
+		while (i < len)
 		{
-            res[i] = save[i];
+			res[i] = *save[i];
 			i++;
 		}
-        res[i] = '\0';
-        temp = ft_strdup(newline + 1);
-        free(*save);
+		res[i] = '\0';
+		temp = ft_strdup(newline + 1);
+		free(*save);
+		*save = temp;
 	}
 }
-static char *tmp(char *start)
-{
-    char *temp;
-    char *newline;
-    temp = ft_strdup(newline + 1);
-    free(*start);
-    *start = temp;
-}
+
 char	*get_next_line(int fd)
 {
 	static char *s;
