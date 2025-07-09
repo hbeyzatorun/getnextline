7693d186739306d89e548b7814d6418e3656fbc5
Huriye Beyza Torun <htorun@k1m11s03.42istanbul.com.tr>
2025-07-09 06:58:05 +0300

    deleted test

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -42,7 +42,7 @@ char	*ft_strdup(const char *s)
 	dup[i] = '\0';
 	return (dup);
 }
-void	*ft_strjoin(const char *s1, char const *s2)
+char	*ft_strjoin(const char *s1, char const *s2)
 {
 	int i;
 	int j;
